e6609d12a14d03dc5456126d2a82330d3080479b
nguyenthanhhuy2711 <huy299193@gmail.com>
2025-06-15 19:44:38 +0700

    cap nhat nut xoa

--- sanpham/xoa.php
+++ sanpham/xoa.php
@@ -1,5 +1,11 @@
 <?php
 include('../includes/connect.php');
 $id = $_GET['id'];
+
+// Xóa ảnh liên quan đến sản phẩm trước
+$conn->query("DELETE FROM anh_bien_the WHERE maSanPham = $id");
+
+// Sau đó xóa sản phẩm
 $conn->query("DELETE FROM san_pham WHERE maSanPham = $id");
+
 header('Location: index.php');
